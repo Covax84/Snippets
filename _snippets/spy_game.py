dff9ba51603e4f48764113ea4dc9b5e4b50f0b7b
Covax <57707580+Covax84@users.noreply.github.com>
2019-12-08 21:19:42 +0300

    Annotations, doc fix.

--- _snippets/spy_game.py
+++ _snippets/spy_game.py
@@ -1,9 +1,9 @@
-def spy_game(arr: list):
-    """ input: list of integers
-        output: true if list contains sequence 0, 0, 7
+def spy_game(arr: list) -> bool:
+    """ Input: list of integers
+        Output: true if list contains sequence 0, 0, 7
     """
     return (''.join(str(x) for x in arr)).find('007') != -1
 
 
-my_list = [0, 0, 1, 2, 3, 0, 0, 2, 0, 0, 7, 1, 2, 3]
-print(spy_game(my_list))
+# my_list = [0, 0, 1, 2, 3, 0, 0, 2, 0, 0, 7, 1, 2, 3]
+# print(spy_game(my_list))
