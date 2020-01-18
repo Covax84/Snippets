6953eccbfd8d50fe86c1413a91fa2bd9e344f236
Covax <57707580+Covax84@users.noreply.github.com>
2020-01-18 19:21:51 +0300

    Examples of using decorator function
    Educational snippet.

---
+++ decorator_functions.py
@@ -0,0 +1,40 @@
+# 1) example of decorator function for original function with no arguments:
+def decorator_function_when_no_args(original_function):
+    def wrapper_function():
+        print('wrapper function executed this before "{}"'.format(original_function.__name__))
+        return original_function()
+    return wrapper_function  # wrapper is waiting to call(we have no call brackets here)
+
+
+# one way to use decorators:
+def display():
+    print('display function executed as variable')
+
+
+example = decorator_function_when_no_args(display)  # now example is variable waiting to call to execute wrapper function
+example()   # wrapper function executed here
+
+
+# and another way to do the same:
+@decorator_function_when_no_args  # this syntax is equal to: display = decorator_function(display); display()
+def display():
+    print('display function executed with @decorator')
+
+
+display()  # every time we call display function it is executed with decorator - wrapper
+
+
+# 2) example of decorator function for original function with arguments:
+def decorator_function(original_function):
+    def wrapper_function(*args, **kwargs):
+        print('wrapper function executed this before "{}"'.format(original_function.__name__))
+        return original_function(*args, **kwargs)
+    return wrapper_function
+
+
+@decorator_function
+def display_with_args(name: str, age: int):
+    print('display function executed with arguments: Name is {}, age is {}'.format(name, age))
+
+
+display_with_args('Covax', 35)
